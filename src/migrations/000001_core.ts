c7f85242051b6a900aca5c4cd76b31d69ca7dcdf
Donovan Dicks <donovan.dicks@outlook.com>
2024-04-02 14:32:45 -0400

    Change migrations setup and content
    Use SQL files and load from disk instead of writing sql strings in
    typescript files.
    
    Add audit table with automatic audit triggers

---
+++ src/migrations/000001_core.ts
@@ -0,0 +1,18 @@
+import { readFileSync } from "fs";
+import { MigrationBuilder } from "node-pg-migrate";
+import { basename, extname, join, resolve } from "path";
+
+const basePath = join(join(resolve(__dirname), "sql"), basename(__filename, extname(__filename)));
+
+const upPath = `${basePath}.up.sql`;
+const downPath = `${basePath}.down.sql`;
+
+export const up = (pgm: MigrationBuilder) => {
+  const sql = readFileSync(upPath, "utf-8");
+  pgm.sql(sql);
+};
+
+export const down = (pgm: MigrationBuilder) => {
+  const sql = readFileSync(downPath, "utf-8");
+  pgm.sql(sql);
+};
---
+++ src/migrations/sql/000001_core.down.sql
@@ -0,0 +1,13 @@
+DROP TRIGGER IF EXISTS trg_set_group_created_at ON tbl_groups;
+DROP TRIGGER IF EXISTS trg_set_user_created_at ON tbl_users;
+DROP TRIGGER IF EXISTS trg_set_group_updateed_at ON tbl_groups;
+DROP TRIGGER IF EXISTS trg_set_user_updated_at ON tbl_users;
+DROP TRIGGER IF EXISTS trg_group_audit_insert ON tbl_groups;
+DROP TRIGGER IF EXISTS trg_user_audit_insert ON tbl_users;
+DROP FUNCTION IF EXISTS fn_set_created_at;
+DROP FUNCTION IF EXISTS fn_set_updated_at;
+DROP FUNCTION IF EXISTS fn_insert_audit_log;
+DROP TABLE IF EXISTS tbl_group_members;
+DROP TABLE IF EXISTS tbl_users;
+DROP TABLE IF EXISTS tbl_groups;
+DROP TABLE IF EXISTS tbl_audit;
