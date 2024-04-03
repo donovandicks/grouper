de7cad3676802f46666915bd413ffe42f9922931
Donovan Dicks <donovan.dicks@outlook.com>
2024-04-03 18:00:44 -0400

    Failing test: confirm gha action fail

---
+++ src/services/datastore/in-memory.unit.test.ts
@@ -0,0 +1,25 @@
+import { CreateGroupDTO } from "../../api/models";
+import { InMemoryDatastore } from "./in-memory";
+
+let ds: InMemoryDatastore;
+
+describe("InMemoryDatastore", () => {
+  beforeEach(() => {
+    ds = new InMemoryDatastore();
+  });
+
+  it("should support creating a group", async () => {
+    // GIVEN
+    const g: CreateGroupDTO = { name: "Test" };
+
+    // WHEN
+    const created = await ds.createGroup(g);
+
+    // THEN
+    expect(created.name).toEqual("Test");
+    expect(created.handle).toEqual("test");
+    expect(created.id.length).toEqual(35);
+    expect(created.createdAt.getDay()).toEqual(new Date().getDay());
+    expect(created.updatedAt.getDay()).toEqual(new Date().getDay());
+  });
+});
