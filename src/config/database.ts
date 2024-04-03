f17e2dbebd7bf8d4f41d70fa4167c9754945f7fd
Donovan Dicks <donovan.dicks@outlook.com>
2024-04-03 00:23:36 -0400

    Parameterize migration and return instead of throw

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,31 +1,34 @@
 import { logger } from "../utils/telemtery";
 import migrate from "node-pg-migrate";
 import { join, resolve } from "path";
-import { ClientConfig, Pool } from "pg";
+import { ClientConfig } from "pg";
 
-export const config: ClientConfig = {
-  user: "username",
-  password: "password",
-  host: "database",
-  port: 5432,
-  database: "grouper",
+export const Config: ClientConfig = {
+  user: process.env["POSTGRES_USER"],
+  password: process.env["POSTGRES_PASSWORD"],
+  host: process.env["POSTGRES_HOST"],
+  port: Number(process.env["POSTGRES_PORT"]),
+  database: process.env["POSTGRES_DB"],
 };
 
-export const client: Pool = new Pool(config);
-
-export async function runMigrations() {
+export async function runMigrations(
+  config: ClientConfig = Config,
+  direction: "up" | "down" = "up",
+  verbose: boolean = true,
+): Promise<boolean> {
   try {
     await migrate({
       schema: "public",
-      direction: "up",
-      verbose: true,
+      direction,
+      verbose,
       dir: join(resolve(__dirname, ".."), "migrations"),
       migrationsTable: "tbl_migrations",
       databaseUrl: config,
     });
     logger.info("migrations ran successfully.");
+    return true;
   } catch (err) {
     logger.error({ err }, "error running migrations");
-    throw err;
+    return false;
   }
 }
