5ff9ed4b3eae9c5679394ba2e47fb30fabdfb8eb
Donovan Dicks <donovan.dicks@outlook.com>
2024-04-22 19:20:15 -0400

    Add new integration tests

---
+++ src/__tests__/integration/constants.ts
@@ -0,0 +1,7 @@
+export const TestConfig = {
+  host: "0.0.0.0",
+  port: 5433,
+  user: "test",
+  password: "password",
+  database: "test",
+};
--- src/__tests__/integration/migrations.spec.ts
+++ src/__tests__/integration/migrations.spec.ts
@@ -1,20 +1,22 @@
 import { runMigrations } from "../../config/database";
+import { EventType } from "../../domain";
+import { InMemoryAc } from "../../services/access-control";
+import { PostgresDatastore } from "../../services/datastore";
 import { Transactor } from "../../services/datastore/postgres/transactor";
+import { GroupService } from "../../services/group/group-service";
+import { UserService } from "../../services/user/user-service";
 import { initLogger } from "../../utils/telemtery";
-import { afterAll, beforeAll, describe, expect, it } from "bun:test";
+import { TestConfig } from "./constants";
+import { sleepSync } from "bun";
+import { afterAll, afterEach, beforeAll, describe, expect, it } from "bun:test";
 import { Pool } from "pg";
 
 initLogger("local");
 
+let tx: Transactor;
+let gs: GroupService;
+let us: UserService;
 let pool: Pool;
-
-const TestConfig = {
-  host: "0.0.0.0",
-  port: 5433,
-  user: "test",
-  password: "password",
-  database: "test",
-};
 
 const expectedTables = [
   "tbl_users",
@@ -24,22 +26,49 @@ const expectedTables = [
   "tbl_groups",
 ];
 
-describe("postgres integration tests", () => {
+const makeCleanTableSql = (name: string): string => {
+  // annoying utility to delete from a table if it exists
+  return `
+  DO $$
+  BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '${name}')
+    THEN
+      DELETE FROM ${name};
+    END IF;
+  END $$
+  `;
+};
+
+describe("service and database integration tests", () => {
   beforeAll(() => {
     pool = new Pool(TestConfig);
+    const ac = new InMemoryAc();
+    const ds = new PostgresDatastore(pool);
+    gs = new GroupService(ac, ds);
+    us = new UserService(ds);
   });
 
   afterAll(async () => {
+    await tx.query("DELETE FROM tbl_migrations;");
     await pool.end();
   });
 
-  it("successfully runs migrations", async () => {
-    const tx = new Transactor(pool);
+  afterEach(async () => {
+    await tx.withTransaction<void>(async () => {
+      await tx.query(makeCleanTableSql("tbl_group_members"));
+      await tx.query(makeCleanTableSql("tbl_group_member_history"));
+      await tx.query(makeCleanTableSql("tbl_users"));
+      await tx.query(makeCleanTableSql("tbl_groups"));
+    });
+  });
+
+  it("successfully runs up migrations", async () => {
+    tx = new Transactor(pool);
 
     const upResult = await runMigrations(pool);
     expect(upResult).toBe(true);
 
-    let tbls = (
+    const tbls = (
       await tx.query(
         `
         SELECT table_schema AS schema, table_name AS name
@@ -54,11 +83,71 @@ describe("postgres integration tests", () => {
     expect(
       tbls.every((tbl: { schema: string; name: string }) => expectedTables.includes(tbl.name)),
     ).toBe(true);
+  });
 
+  it("successfully captures group lifecycle", async () => {
+    // GIVEN
+    const group = await gs.createGroup({ name: "Test" });
+    const user_1 = await us.createUser({ name: "John", email: "john@email.com" });
+    const user_2 = await us.createUser({ name: "Jane", email: "jane@email.com" });
+    const expectedHistoryData = [
+      { type: EventType.Create, data: group },
+      { type: EventType.AddMember, data: { userId: user_1.id } },
+      { type: EventType.AddMember, data: { userId: user_2.id } },
+      { type: EventType.RemoveMember, data: { userId: user_1.id } },
+      { type: EventType.AddMember, data: { userId: user_1.id } },
+    ];
+
+    // WHEN
+    await gs.addMemberToGroup(group.id, user_1.id);
+    sleepSync(5);
+    await gs.addMemberToGroup(group.id, user_2.id);
+    sleepSync(5);
+    await gs.removeMemberFromGroup(group.id, user_1.id);
+    await gs.addMemberToGroup(group.id, user_1.id);
+    const history = await gs.getGroupHistory(group.id);
+
+    // THEN
+    expect(history).not.toBe(undefined);
+    expect(history!.length).toEqual(5);
+    expect(
+      history?.map((h) => {
+        return { type: h.type, data: h.data };
+      }),
+    ).toEqual(expectedHistoryData);
+  });
+
+  it("does not repeat idempotent events", async () => {
+    // GIVEN
+    const group = await gs.createGroup({ name: "Test" });
+    const user = await us.createUser({ name: "John", email: "john@email.com" });
+    const expectedHistoryData = [
+      { type: EventType.Create, data: group },
+      { type: EventType.AddMember, data: { userId: user.id } },
+      { type: EventType.RemoveMember, data: { userId: user.id } },
+    ];
+
+    // WHEN
+    await gs.addMemberToGroup(group.id, user.id);
+    await gs.addMemberToGroup(group.id, user.id);
+    await gs.removeMemberFromGroup(group.id, user.id);
+    await gs.removeMemberFromGroup(group.id, user.id);
+    const history = await gs.getGroupHistory(group.id);
+
+    // THEN
+    expect(history).not.toBe(undefined);
+    expect(
+      history?.map((h) => {
+        return { type: h.type, data: h.data };
+      }),
+    ).toEqual(expectedHistoryData);
+  });
+
+  it("successfully runs down migrations", async () => {
     const downResult = await runMigrations(pool, "down");
     expect(downResult).toBe(true);
 
-    tbls = (
+    const tbls = (
       await tx.query(
         `
         SELECT table_schema AS schema, table_name AS name
