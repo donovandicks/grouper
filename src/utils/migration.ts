44dd18bc815afec894e570013828650edfe5ed86
Donovan Dicks <donovan.dicks@outlook.com>
2024-04-22 19:18:21 -0400

    Light refactor on the migration code

--- src/utils/migration.ts
+++ src/utils/migration.ts
@@ -5,11 +5,22 @@ import { basename, join } from "path";
 import type { Pool } from "pg";
 
 export class Migrator {
+  // TODO: Seriously invest in this or replace with drizzle
   readonly migrationTable: string = "tbl_migrations";
-  upPaths: string[] = [];
-  downPaths: string[] = [];
-  tx: Transactor;
+  readonly upPaths: string[] = [];
+  readonly downPaths: string[] = [];
+  private tx: Transactor;
 
+  /**
+   * Create a new Migrator instance.
+   *
+   * Searches the file system for SQL migration files in the given migration
+   * directory.
+   *
+   * @param pool A configued database pool
+   * @param migrationDir The file directory where SQL migration files are located
+   * @param migrationTable The name of the SQL table to store migration state
+   */
   constructor(pool: Pool, migrationDir: string, migrationTable?: string) {
     if (migrationTable) {
       this.migrationTable = migrationTable;
@@ -19,23 +30,19 @@ export class Migrator {
 
     logger.debug({ migrationDir }, "looking for sql migration files");
 
-    for (const path of readdirSync(migrationDir).sort((a, b) => (a < b ? 0 : 1))) {
-      if (path.endsWith(".up.sql")) {
-        this.upPaths.push(join(migrationDir, path));
-        continue;
-      }
-
-      this.downPaths.unshift(join(migrationDir, path)); // reverse order for down
+    for (const path of readdirSync(migrationDir)) {
+      const fullPath = join(migrationDir, path);
+      path.endsWith(".up.sql") ? this.upPaths.push(fullPath) : this.downPaths.push(fullPath);
     }
 
     // Ensure migrations are sorted by their \d{6} prefix
-    this.upPaths = this.upPaths.sort((a, b) => (a < b ? -1 : 1));
-    this.downPaths = this.downPaths.sort((a, b) => (a < b ? 1 : -1)); // reverse order for down
+    this.upPaths.sort((a, b) => (a < b ? -1 : 1));
+    this.downPaths.sort((a, b) => (a < b ? 1 : -1)); // reverse order for down
 
     logger.debug({ upPaths: this.upPaths, downPaths: this.downPaths }, "found sql migration files");
   }
 
-  async up(): Promise<boolean> {
+  async initMigrations(): Promise<string | null> {
     await this.tx.query("CREATE TABLE IF NOT EXISTS tbl_migrations(name TEXT);");
 
     const lastMigration = (
@@ -46,6 +53,11 @@ export class Migrator {
       logger.info(`found existing migrations table, last migration was '${lastMigration}'`);
     }
 
+    return lastMigration;
+  }
+
+  async up(): Promise<boolean> {
+    const lastMigration = await this.initMigrations();
     const toDo = this.upPaths.filter((mig) => basename(mig) > (lastMigration ?? ""));
 
     if (toDo.length === 0) {
@@ -57,11 +69,6 @@ export class Migrator {
       const sql = readFileSync(mig, "utf-8");
       const name = basename(mig);
 
-      if (name == lastMigration) {
-        logger.debug(`skipping migration '${name}' because it was already run`);
-        continue;
-      }
-
       logger.debug({ migration: name }, "executing migration");
       try {
         await this.tx.query(sql);
@@ -69,8 +76,8 @@ export class Migrator {
         logger.error({ err, migration: name }, "failed to run migration");
         return false;
       }
-
       logger.debug({ migration: name }, "successfully executed migration");
+
       await this.tx.query(`INSERT INTO ${this.migrationTable} (name) VALUES ($1)`, [name]);
     }
 
