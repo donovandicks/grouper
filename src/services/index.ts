831965e38b1b48a6851930fde34d792660a9c99c
Donovan Dicks <donovan.dicks@outlook.com>
2024-05-04 14:19:30 -0400

    Add RuleService

---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+export { GroupService } from "./group";
+export { GroupGenerationService } from "./group-generation";
+export { GroupMemberService } from "./group-member";
+export { RuleService } from "./rule";
+export { UserService } from "./user";
