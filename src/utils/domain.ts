da18f5939b21c632d29538a99e2aaed17ea78445
Donovan Dicks <donovan.dicks@outlook.com>
2024-05-04 19:27:56 -0400

    Domain capitalization utility

--- src/utils/domain.ts
+++ src/utils/domain.ts
@@ -1,3 +1,11 @@
 export const toKebab = (raw: string): string => {
   return raw.toLocaleLowerCase().replaceAll(" ", "-");
 };
+
+export const capWords = (raw: string): string => {
+  return raw
+    .toLocaleLowerCase()
+    .split(" ")
+    .map((w) => w.charAt(0).toLocaleUpperCase() + w.slice(1))
+    .join(" ");
+};
