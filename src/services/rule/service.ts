37167afce2ce9364fdf352ca5c36d474b119efdf
Donovan Dicks <donovan.dicks@outlook.com>
2024-05-04 19:28:12 -0400

    Publish message when a rule is created

--- src/services/rule/service.ts
+++ src/services/rule/service.ts
@@ -1,12 +1,15 @@
 import type { CreateRuleDTO } from "../../api/models";
+import { EventChannels, type Cache } from "../../cache";
 import type { Datastore } from "../../datastore";
 import type { Rule } from "../../domain/rule";
 
 export class RuleService {
   private db: Datastore;
+  private cache: Cache;
 
-  constructor(db: Datastore) {
+  constructor(db: Datastore, cache: Cache) {
     this.db = db;
+    this.cache = cache;
   }
 
   async listRules(): Promise<Rule[]> {
@@ -18,6 +21,8 @@ export class RuleService {
   }
 
   async createRule(rule: CreateRuleDTO): Promise<Rule> {
-    return this.db.createRule(rule);
+    const created = await this.db.createRule(rule);
+    await this.cache.publish(EventChannels.RuleCreated, JSON.stringify(created));
+    return created;
   }
 }
