be1545533bbf96cd2d1dee10e769e0fc0ec431ee
Donovan Dicks <donovan.dicks@outlook.com>
2024-05-08 19:03:42 -0400

    Adjust condition domain modeling

--- src/domain/condition.ts
+++ src/domain/condition.ts
@@ -8,13 +8,12 @@ export type Condition = {
 };
 
 // [T, ...T[]] enforces at least one element in the array
-export type ConditionList = [Condition, ...Condition[]];
-export type ConditionMapList = [ConditionMap, ...ConditionMap[]];
+export type ConditionOrMapList = [Condition | ConditionMap, ...(Condition | ConditionMap)[]];
 
 // Hacky "one or the other but not both" implementation
 export type ConditionMap =
-  | { and: ConditionList | ConditionMapList; or?: never }
-  | { or: ConditionList | ConditionMapList; and?: never };
+  | ({ and: ConditionOrMapList } & { or?: never })
+  | ({ or: ConditionOrMapList } & { and?: never });
 
 export const conditionIsSimple = (cond: Condition | ConditionMap): cond is Condition => {
   return "attribute" in cond && "operation" in cond;
