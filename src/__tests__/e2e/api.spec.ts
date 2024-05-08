d8208e16cf5d8f8186f85f5d8f309e2101311c93
Donovan Dicks <donovan.dicks@outlook.com>
2024-05-08 19:03:01 -0400

    Extend API E2E tests

--- src/__tests__/e2e/api.spec.ts
+++ src/__tests__/e2e/api.spec.ts
@@ -1,9 +1,14 @@
+import type { CreateRuleDTO } from "../../api/models";
 import { createGroup, getGroup } from "../../lib";
 import { addGroupMember } from "../../lib/add-group-member";
+import { createRule } from "../../lib/create-rule";
 import { deleteGroup } from "../../lib/delete-group";
+import { deleteAllRules } from "../../lib/delete-rule";
 import { getGroupMembers } from "../../lib/get-group-members";
 import { getOrCreateGroup } from "../../lib/get-or-create-group";
 import { getOrCreateUser } from "../../lib/get-or-create-user";
+import { getRule } from "../../lib/get-rule";
+import { listRules } from "../../lib/list-rules";
 import { queryGroups } from "../../lib/query-group";
 import { queryGroupMembers } from "../../lib/query-group-members";
 import { initLogger } from "../../utils/telemtery";
@@ -18,7 +23,7 @@ describe("end to end suite", () => {
 
   it("successfully creates groups via API", async () => {
     // GIVEN
-    const params = { name: "Test" };
+    const params = { name: "Test", userManaged: true };
 
     // WHEN
     const created = await createGroup(params);
@@ -35,7 +40,7 @@ describe("end to end suite", () => {
 
   it("successfully lists all group members", async () => {
     // GIVEN
-    const group_params = { name: "Test" };
+    const group_params = { name: "Test", userManaged: true };
     const user_1_params = { name: "Alex", email: "alex@email.com" };
     const user_2_params = { name: "Celine", email: "celine@email.com" };
 
@@ -56,7 +61,7 @@ describe("end to end suite", () => {
 
   it("successfully filters group members by email", async () => {
     // GIVEN
-    const group_params = { name: "Test" };
+    const group_params = { name: "Test", userManaged: true };
     const user_1_params = { name: "Alex", email: "alex@email.com" };
     const user_2_params = { name: "Celine", email: "celine@email.com" };
 
@@ -77,7 +82,7 @@ describe("end to end suite", () => {
 
   it("successfully filters group members by name", async () => {
     // GIVEN
-    const group_params = { name: "Test" };
+    const group_params = { name: "Test", userManaged: true };
     const user_1_params = { name: "Alex", email: "alex@email.com" };
     const user_2_params = { name: "Celine", email: "celine@email.com" };
 
@@ -98,8 +103,8 @@ describe("end to end suite", () => {
 
   it("successfully filters groups by name", async () => {
     // GIVEN
-    const group_1_params = { name: "Gold" };
-    const group_2_params = { name: "Silver" };
+    const group_1_params = { name: "Gold", userManaged: true };
+    const group_2_params = { name: "Silver", userManaged: true };
 
     // WHEN
     const group_1 = await getOrCreateGroup(group_1_params);
@@ -118,8 +123,8 @@ describe("end to end suite", () => {
 
   it("successfully filters groups by handle", async () => {
     // GIVEN
-    const group_1_params = { name: "Gold" };
-    const group_2_params = { name: "Silver" };
+    const group_1_params = { name: "Gold", userManaged: true };
+    const group_2_params = { name: "Silver", userManaged: true };
 
     // WHEN
     const group_1 = await getOrCreateGroup(group_1_params);
@@ -134,5 +139,107 @@ describe("end to end suite", () => {
 
     // THEN
     expect(results[0]).toEqual(group_2);
+  });
+
+  it("successfully creates rules", async () => {
+    // GIVEN
+    const rule: CreateRuleDTO = {
+      name: "My Test Rule",
+      description: "A test rule",
+      condition: {
+        and: [
+          {
+            attribute: "team",
+            operation: "equals",
+            value: "Gold Team",
+          },
+          {
+            attribute: "title",
+            operation: "contains",
+            value: "Engineer",
+          },
+        ],
+      },
+    };
+
+    // WHEN
+    const created = await createRule(rule);
+
+    // THEN
+    expect(typeof created.id).toBe("string");
+    expect(created.createdAt).not.toBeEmpty();
+    expect(created.name).toEqual(rule.name);
+    expect(created.description).toEqual(rule.description);
+    expect(created.condition).toEqual(rule.condition);
+  });
+
+  it("successfully retrieves rules by ID", async () => {
+    // GIVEN
+    const rule: CreateRuleDTO = {
+      name: "My Other Test Rule",
+      description: "Another test rule",
+      condition: {
+        or: [
+          {
+            attribute: "title",
+            operation: "equals",
+            value: "Software Engineer",
+          },
+          {
+            attribute: "title",
+            operation: "equals",
+            value: "Systems Engineer",
+          },
+        ],
+      },
+    };
+
+    // WHEN
+    const created = await createRule(rule);
+    const received = await getRule(created.id);
+
+    // THEN
+    expect(received).toEqual(created);
+  });
+
+  it("successfully lists rules", async () => {
+    // GIVEN
+    const rule: CreateRuleDTO = {
+      name: "Yet Another Test Rule",
+      description: "Indeed, another test rule",
+      condition: {
+        and: [
+          {
+            attribute: "title",
+            operation: "equals",
+            value: "Software Engineer",
+          },
+          {
+            or: [
+              {
+                attribute: "level",
+                operation: "equals",
+                value: "1",
+              },
+              {
+                attribute: "level",
+                operation: "equals",
+                value: "2",
+              },
+            ],
+          },
+        ],
+      },
+    };
+
+    await deleteAllRules();
+
+    // WHEN
+    const created = await createRule(rule);
+    const received = await listRules();
+
+    // THEN
+    expect(received.length).toEqual(1);
+    expect(received[0]).toEqual(created);
   });
 });
